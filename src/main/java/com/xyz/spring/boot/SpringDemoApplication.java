3798571652f2ac16eae81451508bf4d9c27ffccf
kuanghuan <huankuang@alog.com>
2017-08-15 10:29:50 +0800

    add Swagger
    Signed-off-by: kuanghuan <huankuang@alog.com>

--- src/main/java/com/xyz/spring/boot/SpringDemoApplication.java
+++ src/main/java/com/xyz/spring/boot/SpringDemoApplication.java
@@ -2,16 +2,34 @@ package com.xyz.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @ServletComponentScan("com.xyz.spring.boot")
 @ComponentScan("com.xyz.spring.boot")
-public class SpringDemoApplication {
+public class SpringDemoApplication implements EmbeddedServletContainerCustomizer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		//container.setPort(9000);
+	}
+	
+	@Bean
+	public TomcatEmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+		factory.setPort(9001);
+		return factory;
 	}
 	
 }
--- src/main/java/com/xyz/spring/boot/controller/IndexController.java
+++ src/main/java/com/xyz/spring/boot/controller/IndexController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wordnik.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/")
 public class IndexController {
 	
+	@ApiOperation(value = "首页", httpMethod = "GET", notes = "获取首页数据", response = Map.class)
 	@GetMapping(value = "/index")
 	public Map<String, Object> index() {
 		Map<String, Object> retMap = new HashMap<String, Object>();
